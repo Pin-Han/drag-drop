ec082d4d12c98e2682b1c4f893dcb98784b71c0c
Patrick <patrick04184@gmail.com>
2021-11-10 00:30:03 +0800

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,23 @@
+
 const path = require('path');
+
 module.exports = {
-  entry: './src/app.ts',
+  entry: './src/app.ts', //bundle 的進入點
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: 'bundle.js',  // bundle 檔案名稱
+    path: path.resolve(__dirname, 'dist') //bundle 檔案的位置
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
   }
-}
+};
